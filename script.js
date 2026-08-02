66e78a800979f1ec3aeed40d8151cc86916f7716
elbaifo <alvaroo.fernandezz7@gmail.com>
2026-08-02 07:15:30 +0200

    Crear script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    iniciarAplicacion();
+
+});
+
+function iniciarAplicacion(){
+
+    const app = document.getElementById("app");
+
+    app.innerHTML = "";
+
+}
