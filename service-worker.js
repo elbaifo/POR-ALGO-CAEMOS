415e6a86239627b67bbefa2f802e31a66ee0868c
elbaifo <alvaroo.fernandezz7@gmail.com>
2026-08-02 09:51:49 +0200

    Crear service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "por-algo-creamos-v1";
+
+const ARCHIVOS = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
+    "./favicon.png"
+];
+
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+        .then(cache => {
+
+            return cache.addAll(ARCHIVOS);
+
+        })
+
+    );
+
+});
+
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
