fb92a21c70a79a16d5d10e0e8a2482988a7977be
elbaifo <alvaroo.fernandezz7@gmail.com>
2026-08-04 14:49:04 +0200

    Actualizar spiderflappy.js

--- spiderflappy.js
+++ spiderflappy.js
@@ -34,36 +34,39 @@ function saveProfile(profile){
 }
 
 
-let profile = getProfile();
-cargarCosmeticosEquipados();
-
-const spiderSubida = new Image();
-spiderSubida.src = "spidersubida.png";
-
-
-const spiderMedio = new Image();
-spiderMedio.src = "spidermedio.png";
-
-
-const spiderCaida = new Image();
-spiderCaida.src = "spidercaida.png";
-
-
-const edificioNoche = new Image();
-
-const ciudadNoche = new Image();
-
-
 function cargarCosmeticosEquipados(){
 
     const perfil = getProfile();
-
 
     if(!perfil || !perfil.equipado){
         edificioNoche.src = "edificio_noche.png";
         ciudadNoche.src = "ciudad_noche.png";
         return;
     }
+
+    const edificios = {
+        edificio_noche:"edificio_noche.png",
+        edificio_dia:"edificio_dia.png"
+    };
+
+    const fondos = {
+        ciudad_noche:"ciudad_noche.png",
+        ciudad_dia:"ciudad_dia.png"
+    };
+
+    edificioNoche.src =
+    edificios[perfil.equipado.edificio]
+    || "edificio_noche.png";
+
+
+    ciudadNoche.src =
+    fondos[perfil.equipado.fondo]
+    || "ciudad_noche.png";
+
+}
+
+
+cargarCosmeticosEquipados();
 
 
     const edificios = {
